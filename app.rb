682a9024f0b22f1a22792b341aaf9445b6f3c669
Drew <drew.r.wilson@gmail.com>
2014-06-07 15:38:09 -0600

    inital working version

--- app.rb
+++ app.rb
@@ -1,19 +1,24 @@
 require 'rubygems'
 require 'sinatra'
-require 'haml'
+
+password = 'test'
 
 # Handle GET-request (Show the upload form)
 get "/upload" do
   File.read(File.join('views', 'upload.html'))
 end
 
+get "/" do
+  File.read(File.join('views', 'index.html'))
+end
+
 # Handle POST-request (Receive and save the uploaded file)
 post "/upload" do
-  if params['password'] == 'pw1'
+  if params['password'] == password
     File.open('uploads/' + params['myfile'][:filename], "w") do |f|
       f.write(params['myfile'][:tempfile].read)
     end
-    return "The file was successfully uploaded!"
+    redirect '/?success=1'
   end
   # else return wrong password:
   return "Password wrong!"
